bb5a58259644019971f447f2c8ddc7213ef62f42
Johnson Chen <poshenc@gmail.com>
2018-08-25 21:19:47 +0800

    qr code mouseover added

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,11 +5,3 @@ document.getElementById("btn1").addEventListener("click", function() {
 document.querySelector(".close").addEventListener("click", function() {
   document.querySelector(".bg-modal").style.display = "none";
 });
-
-document.getElementById("btn2").addEventListener("click", function() {
-  document.querySelector(".bg-modal-2").style.display = "flex";
-});
-
-document.querySelector(".close2").addEventListener("click", function() {
-  document.querySelector(".bg-modal-2").style.display = "none";
-});
